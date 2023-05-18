548b71ee3642be3a53abf7263c9777236ae3c8c5
misha.stefantsiv <mwalkman21@gmail.com>
2023-05-18 14:24:40 +0200

    scripts modified

--- specs/hello.py
+++ specs/hello.py
@@ -1,8 +1,17 @@
-import import_test as test
+import os
+
+
+def get_function_name():
+    function_name = os.environ.get('FUNCTION_NAME')
+    if function_name is None:
+        raise EnvironmentError("Unable to retrieve function name.")
+    return function_name
 
 
 def main():
-    test.test()
+    # Usage
+    function_name = get_function_name()
+    print("Function name:", function_name)
 
 
 if __name__ == "__main__":
