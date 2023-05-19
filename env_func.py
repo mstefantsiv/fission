d4944070fb6d0f926c82e032a084b1a7a82da7b0
misha.stefantsiv <mwalkman21@gmail.com>
2023-05-19 15:12:21 +0200

    file structure changed script updated keda yaml added prepare edited

--- env_func.py
+++
@@ -1,4 +0,0 @@
-import os
-
-output = os.environ.get('OUTPUT_STRING', 'No data')
-print(output)
--- specs/hello.py
+++ specs/hello.py
@@ -1,17 +1,9 @@
-import os
-
-
 def get_function_name():
-    function_name = os.environ.get('FUNCTION_NAME')
-    if function_name is None:
-        raise EnvironmentError("Unable to retrieve function name.")
-    return function_name
+    print("Function executed")
 
 
 def main():
-    # Usage
-    function_name = get_function_name()
-    print("Function name:", function_name)
+    get_function_name()
 
 
 if __name__ == "__main__":
