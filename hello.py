1ce43d001d7f74b2f54e009de3c021924d8e4470
Misha Stefantsiv <mwalkman21@gmail.com>
2023-06-14 17:47:28 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,4 @@
-from flask import requests
 
-def main():
-    current_app.logger.info("Received request")
-    msg = "--BODY--\n%s\n-----\n" % (request.get_data())
+def main(context)
+    msg = "--BODY--\n%s\n-----\n" % (str(context))
     return msg
